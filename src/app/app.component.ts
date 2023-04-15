bff510d47990e600073c75db41b38d645c81d7ec
kabbach55 <kabbach_zakaria@emsi-edu.ma>
2023-04-15 15:17:06 +0000

    creation project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CatalogueService} from "./catalogue.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ecom-front';
+export class AppComponent implements OnInit{
+   products:any ;
+  constructor(private catService:CatalogueService) {
+  }
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  private getProducts() {
+    this.catService.getResource("/products").subscribe(data=>{
+      this.products=data;
+    },error => {
+      console.log(error);
+      }
+    );
+  }
 }
